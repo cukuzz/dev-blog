053411e119537190fbfd195aeec9675e5ef7d29d
gul-mac <jangul@gul-mac.local>
2022-02-07 19:35:18 +0900

    feat:add header,side menu
    - modify layout(header, side menu)
    - use react-bootstrap

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,46 +1,22 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
+import SideMenu from './components/SideMenu'
 
-interface Props {
+interface IProps {
   siteTitle: string
 }
 
-const Header = ({ siteTitle }: Props) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = (props: IProps) => {
+  const { siteTitle } = props
+  return (
+    <Navbar bg="light" expand={false}>
+      <Container fluid>
+        <Navbar.Brand href="#">{siteTitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <SideMenu siteTitle={siteTitle} />
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
